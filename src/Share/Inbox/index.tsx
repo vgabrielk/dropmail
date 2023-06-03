b37232c973df6ea6cf751c408a9b2bef655063cf
vgabrielk <vfelix1553@gmail.com>
2023-06-03 10:40:06 -0300

    improve: Customização no css do MaterialUI  para melhor experiência do usuário

--- src/Share/Inbox/index.tsx
+++ src/Share/Inbox/index.tsx
@@ -4,15 +4,44 @@ import { Item } from "../../components/ItemComponent";
 
 const Inbox = () => {
 
-  
 
   return (
     <Fragment>
       <Stack spacing={0}>
-        <Item sx={{ margin: '0 3rem' }}>
-
+        <Item sx={{ margin: '0 3rem', marginBottom: '2rem' }}>
+          <h1 style={{ paddingBottom: '2rem' }}>Inbox</h1>
           <Grid container spacing={2} columns={16}>
-            <Grid sm={16} md={8}  >
+            <Grid item sm={16} md={8} sx={{ overflowY: 'scroll', height: '200px', marginBottom: '20px' }}  >
+              <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                border: '1px solid #e0e0e0',
+                padding: 2,
+                boxSizing: 'border-box',
+              }}>
+                <h4>Subject</h4>
+                <p>Your temp email add is ready...</p>
+              </Box>
+              <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                border: '1px solid #e0e0e0',
+                padding: 2,
+                boxSizing: 'border-box'
+              }}>
+                <h4>Subject</h4>
+                <p>Your temp email add is ready...</p>
+              </Box>
+              <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                border: '1px solid #e0e0e0',
+                padding: 2,
+                boxSizing: 'border-box',
+              }}>
+                <h4>Subject</h4>
+                <p>Your temp email add is ready...</p>
+              </Box>
               <Box sx={{
                 display: 'flex',
                 flexDirection: 'column',
@@ -24,7 +53,7 @@ const Inbox = () => {
                 <p>Your temp email add is ready...</p>
               </Box>
             </Grid>
-            <Grid sm={16} md={8}  >
+            <Grid item sm={16} md={8}  >
               <Box sx={{
                 display: 'flex',
                 flexDirection: 'column',
